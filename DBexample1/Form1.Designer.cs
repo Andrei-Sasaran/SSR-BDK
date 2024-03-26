3365b960eddd1b5b68c198b6f363900117c76bf1
Badarau Cristian-Andrei <badarau.so.cristian@student.utcluj.ro>
2024-03-26 22:08:30 +0200

    Top

---
+++ DBexample1/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace DBexample1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox_Univ = new System.Windows.Forms.ListBox();
+            this.textBox_City = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_CodeUni = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox_Fac = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox_Fac);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox_CodeUni);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox_City);
+            this.groupBox1.Controls.Add(this.listBox_Univ);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(476, 476);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // listBox_Univ
+            // 
+            this.listBox_Univ.FormattingEnabled = true;
+            this.listBox_Univ.ItemHeight = 16;
+            this.listBox_Univ.Location = new System.Drawing.Point(6, 21);
+            this.listBox_Univ.Name = "listBox_Univ";
+            this.listBox_Univ.Size = new System.Drawing.Size(230, 324);
+            this.listBox_Univ.TabIndex = 0;
+            this.listBox_Univ.SelectedIndexChanged += new System.EventHandler(this.listBox_Univ_SelectedIndexChanged);
+            // 
+            // textBox_City
+            // 
+            this.textBox_City.Location = new System.Drawing.Point(242, 257);
+            this.textBox_City.Name = "textBox_City";
+            this.textBox_City.Size = new System.Drawing.Size(228, 22);
+            this.textBox_City.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(242, 238);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Oras:";
+            // 
+            // textBox_CodeUni
+            // 
+            this.textBox_CodeUni.Location = new System.Drawing.Point(242, 323);
+            this.textBox_CodeUni.Name = "textBox_CodeUni";
+            this.textBox_CodeUni.Size = new System.Drawing.Size(228, 22);
+            this.textBox_CodeUni.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(242, 304);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Cod Universitate:";
+            // 
+            // listBox_Fac
+            // 
+            this.listBox_Fac.FormattingEnabled = true;
+            this.listBox_Fac.ItemHeight = 16;
+            this.listBox_Fac.Location = new System.Drawing.Point(242, 21);
+            this.listBox_Fac.Name = "listBox_Fac";
+            this.listBox_Fac.Size = new System.Drawing.Size(230, 196);
+            this.listBox_Fac.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 500);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_CodeUni;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_City;
+        private System.Windows.Forms.ListBox listBox_Univ;
+        private System.Windows.Forms.ListBox listBox_Fac;
+    }
+}
+
